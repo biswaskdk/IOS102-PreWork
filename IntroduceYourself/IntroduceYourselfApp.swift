f47d4e5ef88ab5adc3cc43646972537bef44adef
Bishwash Khadka <biswaskhadka10@gmail.com>
2025-08-27 16:22:48 -0700

    Initial Commit

---
+++ IntroduceYourself/IntroduceYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduceYourselfApp.swift
+//  IntroduceYourself
+//
+//  Created by Bipu on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroduceYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntroduceYourselfTests/IntroduceYourselfTests.swift
@@ -0,0 +1,16 @@
+//
+//  IntroduceYourselfTests.swift
+//  IntroduceYourselfTests
+//
+//  Created by Bipu on 8/27/25.
+//
+
+import Testing
+
+struct IntroduceYourselfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
